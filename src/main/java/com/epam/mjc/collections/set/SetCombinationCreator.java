b669b2b01152aa7643a41bba0923405e56d5d419
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2023-12-26 03:48:07 +0600

    SetCombinationCreator solution

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -4,30 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetCombinationCreator {
-    public static void main(String[] args) {
-        SetCombinationCreator test = new SetCombinationCreator();
-        test.createSetCombination(
-//                new HashSet<>(Set.of("Java", "Collection", "framework", "interface", "class", "Queue")),
-//                new HashSet<>(Set.of("Queue", "iterator", "Java", "Collection", "comparator")),
-//                new HashSet<>(Set.of("Java", "Set", "Map", "List"))
-                new HashSet<>(Set.of("Java", "SortedSet", "Map", "Collections", "Iterable", "Set", "NavigableSet")),
-                new HashSet<>(Set.of("Queue", "SortedSet", "NavigableSet", "List", "Set", "Iterator", "Comparator")),
-                new HashSet<>(Set.of("Java", "Iterable", "Comparator"))
-//                "Collection", "Queue", "Set", "Map", "List"
-        );
+    public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> resultSet = new HashSet<>();
+
+        for (String element : firstSet) {
+            if (secondSet.contains(element) && !thirdSet.contains(element)) {
+                resultSet.add(element);
+            }
+        }
+
+        for (String element : thirdSet) {
+            if (!firstSet.contains(element) && !secondSet.contains(element)) {
+                resultSet.add(element);
+            }
+        }
+        System.out.println(resultSet);
+        return resultSet;
     }
 
-    public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        Set<String> result = new HashSet<>();
-        firstSet.removeAll(thirdSet);
-        firstSet.retainAll(secondSet);
-//        System.out.println(firstSet);
-        thirdSet.removeAll(secondSet);
-        thirdSet.removeAll(firstSet);
-//        System.out.println(thirdSet);
-        result.addAll(firstSet);
-        result.addAll(thirdSet);
-        System.out.println(result);
-        return result;
-    }
 }
